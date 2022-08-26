7cda5fd6a3b29b5a0eec61b532c0966018777ca1
Fedorenko Elias <FedorenkoElias@yandex.ru>
2022-08-26 17:46:53 +0300

    Tilemap

--- src/TestState.hx
+++ src/TestState.hx
@@ -1,5 +1,8 @@
 package;
 
+import drcJS.display.Region;
+import drcJS.display.Tile;
+import drcJS.display.Tilemap;
 import drcJS.utils.Common;
 import drcJS.display.Image;
 import drc.objects.State;
@@ -18,5 +21,20 @@ class TestState extends State {
         var image:Image = new Image(Common.resources.getProfile('res/profiles/texture.json'), [Common.resources.getTexture('res/graphics/grid_bw.png')]);
 
         addGraphic(image);
+
+        var tilemap:Tilemap = new Tilemap(Common.resources.getProfile('res/profiles/texture.json'), [Common.resources.getTexture('res/graphics/grid_mt.png')]);
+
+        tilemap.tileset.addRegion(new Region([0, 0, 32, 32]));
+
+        var _x:Int = 0;
+
+        for (i in 0...10) {
+
+            tilemap.addTile(new Tile(tilemap, 0, _x, 300));
+
+            _x += 64;
+        }
+
+        addGraphic(tilemap);
     }
 }
